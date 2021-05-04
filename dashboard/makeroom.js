fe8433878333998ff4361927fe699b1d40956af1
giniya <giniya@mit.edu>
2021-05-04 15:04:17 -0400

    Changes to formatting

--- dashboard/makeroom.js
+++ dashboard/makeroom.js
@@ -4,8 +4,11 @@ const makeroom = (roomnum) => {
     const newroom = document.createElement("div");
     newroom.classList.add("roombox");
     newroom.classList.add("roombox");
-    // newroom.innerText = `Test room number ${roomnum}`;
-    newroom.innerHTML = "<div>" + `Test room number ${roomnum}` + "</div>" +
+    // newroom.innerText = `Test room number ${roomnum} \\`;
+    newroom.innerHTML = "<div>" + `Test room number ${roomnum} \r\n` +
+			`Capacity: 10/10 \r\n` + 
+			`Current noise level: Quiet \r\n` +
+			`Volume Preferences: Quiet \r\n` + "</div>" +
                         `<button class="checkin"
                             type="button"
                             value="Check In"
