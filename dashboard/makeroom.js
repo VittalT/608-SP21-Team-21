f52e4257ec61a546bacb10b4e2399b837fe02839
Albert B Xing <axing@mit.edu>
2021-05-10 00:16:50 -0400

    add navigation from dashboard to checkinpage

--- dashboard/makeroom.js
+++ dashboard/makeroom.js
@@ -15,3 +15,20 @@ const makeroom = (roomnum) => {
                         > Check In </button>`
     dashBoard.appendChild(newroom);
 }
+
+const checkin = (roomnum) => {
+    const HTTP = new XMLHttpRequest();
+    const url='http://608dev-2.net/sandbox/sc/team21/Server-Side/User-Server-API.py?task=checkinpage';
+    HTTP.open("GET", url);
+    HTTP.send();
+    HTTP.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status==200) {
+            console.log(HTTP.responseText);
+            const body = document.body;
+            body.innerHTML = HTTP.responseText;
+            // TODO: below code to be implemented after the Check In page is created.
+            //roomnum = document.getElementById("roomnum")
+            //roomnum.innerText = roomnum
+        }
+    }
+}
