bf68214a54857c0989338e077af24e565265567f
Albert B Xing <axing@mit.edu>
2021-05-11 14:28:31 -0400

    added checkin button functionality

--- UI/Checkin/checkin.js
+++ UI/Checkin/checkin.js
@@ -3,14 +3,15 @@ if (typeof checkin === 'undefined') {
     checkin = (roomno, hoursstay, noise) => {
         const HTTP = new XMLHttpRequest();
         const url='http://608dev-2.net/sandbox/sc/team21/Server/APIs/UserServerAPI.py?task=checkin';
-        const data={
-            roomnum: roomno,
-            hours: hoursstay,
-            noiselevel: noise
-        };
+        const data=`roomnum=${roomno}&hours=${hoursstay}&noiselevel=${noise}`
+        // {
+        //     roomnum: roomno,
+        //     hours: hoursstay,
+        //     noiselevel: noise
+        // };
         dataType='JSON';
         HTTP.open("POST", url);
-        HTTP.setRequestHeader("Content-type", "application/json");
+        HTTP.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
         HTTP.send(data);
         HTTP.onreadystatechange = function(){
             if(this.readyState == 4 && this.status==200) {
