eafc3165cf11325b83e2d2440a16673437175fbe
Albert B Xing <axing@mit.edu>
2021-05-11 01:41:01 -0400

    added navigation to and from checkinpage, loginpage, friendspage

--- UI/Check in/checkin.js
+++ UI/Check in/checkin.js
@@ -1,10 +1,10 @@
-const login = (roomno, hoursstay, noise) => {
+const checkin = (roomno, hoursstay, noise) => {
     const HTTP = new XMLHttpRequest();
-    const url='http://608dev-2.net/sandbox/sc/team21/Server-Side/User-Server-API.py?task=login';
+    const url='http://608dev-2.net/sandbox/sc/team21/Server-Side/User-Server-API.py?task=checkin';
 	const data={
-		room: roomno,
+		roomnum: roomno,
 		hours: hoursstay,
-		noiselvl: noise
+		noiselevel: noise
 	};
 	dataType='JSON';
     HTTP.open("POST", url);
@@ -13,11 +13,23 @@ const login = (roomno, hoursstay, noise) => {
     HTTP.onreadystatechange = function(){
         if(this.readyState == 4 && this.status==200) {
             console.log(HTTP.responseText);
-            const body = document.body;
-            body.innerHTML = HTTP.responseText;
+            const doc = document.documentElement;
+            // doc.innerHTML = HTTP.responseText;
+            setInnerHTML(doc, HTTP.responseText);
             // TODO: below code to be implemented after the Check In page is created.
             //roomnum = document.getElementById("roomnum")
             //roomnum.innerText = roomnum
         }
     }
 }
+
+var setInnerHTML = function(elm, html) {
+    elm.innerHTML = html;
+    Array.from(elm.querySelectorAll("script")).forEach( oldScript => {
+      const newScript = document.createElement("script");
+      Array.from(oldScript.attributes)
+        .forEach( attr => newScript.setAttribute(attr.name, attr.value) );
+      newScript.appendChild(document.createTextNode(oldScript.innerHTML));
+      oldScript.parentNode.replaceChild(newScript, oldScript);
+    });
+  }
--- UI/Login/login.js
+++ UI/Login/login.js
@@ -12,8 +12,9 @@ const login = (username, pass) => {
     HTTP.onreadystatechange = function(){
         if(this.readyState == 4 && this.status==200) {
             console.log(HTTP.responseText);
-            const body = document.body;
-            body.innerHTML = HTTP.responseText;
+            const doc = document.documentElement;
+            // doc.innerHTML = HTTP.responseText;
+            setInnerHTML(doc, HTTP.responseText);
             // TODO: below code to be implemented after the Check In page is created.
             //roomnum = document.getElementById("roomnum")
             //roomnum.innerText = roomnum
@@ -21,14 +22,14 @@ const login = (username, pass) => {
     }
 }
 
-const login = (username, pass1, pass2) => {
+const createaccount = (username, pass1, pass2) => {
 	if (pass1 != pass2)
 		//Do something about it
 		pass = pass1 //For now
 	else
 		pass = pass1
     const HTTP = new XMLHttpRequest();
-    const url='http://608dev-2.net/sandbox/sc/team21/Server-Side/User-Server-API.py?task=createacct';
+    const url='http://608dev-2.net/sandbox/sc/team21/Server/UserServerAPI.py?task=createaccount';
 	const data={
 		user: username,
 		password: pass
@@ -40,11 +41,23 @@ const login = (username, pass1, pass2) => {
     HTTP.onreadystatechange = function(){
         if(this.readyState == 4 && this.status==200) {
             console.log(HTTP.responseText);
-            const body = document.body;
-            body.innerHTML = HTTP.responseText;
+            const doc = document.documentElement;
+            // doc.innerHTML = HTTP.responseText;
+            setInnerHTML(doc, HTTP.responseText);
             // TODO: below code to be implemented after the Check In page is created.
             //roomnum = document.getElementById("roomnum")
             //roomnum.innerText = roomnum
         }
     }
 }
+
+var setInnerHTML = function(elm, html) {
+    elm.innerHTML = html;
+    Array.from(elm.querySelectorAll("script")).forEach( oldScript => {
+      const newScript = document.createElement("script");
+      Array.from(oldScript.attributes)
+        .forEach( attr => newScript.setAttribute(attr.name, attr.value) );
+      newScript.appendChild(document.createTextNode(oldScript.innerHTML));
+      oldScript.parentNode.replaceChild(newScript, oldScript);
+    });
+  }
